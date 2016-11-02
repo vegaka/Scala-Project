d098cc2c796ab19c67e543beb9ff262a55fac473
Mugthakk <hanakmumakil23@gmail.com>
2016-11-02 14:55:18 +0100

    Account good apart from imports

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,5 +1,24 @@
+import main.scala.exceptions.IllegalAmountException
+import main.scala.exceptions.NoSufficientFundsExceptions
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+	
+	var balance: Double = initialBalance
+
+	def withdraw(amount: Double): Unit = {
+		if (amount > balance){
+			throw new NoSufficientFundsExceptions("Insufficient funds.")
+		} else {
+			balance -= amount
+		}
+	}
+
+	def deposit(amount: Double): Unit = {
+		if (amount < 0){
+			throw new IllegalAmountException("Cannot deposit negative amount.")
+		} else {
+			balance += amount
+		}
+	}
+	def getBalanceAmount: Double = balance
 }
