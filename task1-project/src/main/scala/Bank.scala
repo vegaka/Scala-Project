948cc4d84e5671d5a6787dffc54b10f286b07f4b
Mugthakk <hanakmumakil23@gmail.com>
2016-11-02 15:03:12 +0100

    Add bank-functionality with exception

--- task1-project/src/main/scala/Bank.scala
+++ task1-project/src/main/scala/Bank.scala
@@ -1,10 +1,19 @@
+import exceptions.NoSufficientFundsException
+
 object Bank {
 
-  private var idCounter: Int = 0
+  private var idCounter: AtomInt = 0
 
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? // Implement
+  def transaction(from: Account, to: Account, amount: Double): Unit = {
+  	try{
+  		from.withdraw(amount)
+  		to.deposit(amount)
+  	}catch{
+  		case e: NoSufficientFundsException => NoSufficientFundsException("No sufficient fund, transaction aborted")
+  	}
+  }
 
-  def getUniqueId: Int = {
+  def getUniqueId: AtomInt = {
     idCounter += 1 // Can this be improved?
     idCounter
   }
