c70c1bebc4174671e43af66c98a5de1964fcf41e
Vegard Seim Karstang <vegard.karstang@gmail.com>
2016-11-03 13:31:12 +0100

    AccountTests now succeed.

--- task2-project/src/main/scala/Bank.scala
+++ task2-project/src/main/scala/Bank.scala
@@ -1,37 +1,40 @@
 import java.util.concurrent.atomic.AtomicInteger
 
 import scala.annotation.tailrec
-import scala.concurrent.forkjoin.ForkJoinPool
+import scala.concurrent._
 
 class Bank(val allowedAttempts: Integer = 3) {
 
-  private val uid = new AtomicInteger(0)
-  private val transactionsQueue: TransactionQueue = new TransactionQueue()
-  private val processedTransactions: TransactionQueue = new TransactionQueue()
-  private val executorContext = ???
+    private val uid = new AtomicInteger(0)
+    private val transactionsQueue: TransactionQueue = new TransactionQueue()
+    private val processedTransactions: TransactionQueue = new TransactionQueue()
+    private val executorContext = ExecutionContext.global
 
-  def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
-    transactionsQueue push new Transaction(
-      transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
-  }
+    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
+        transactionsQueue push new Transaction(
+            transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
+    }
 
-  @tailrec final def generateAccountId: Int = {
+    @tailrec final def generateAccountId: Int = {
         val current = uid.get
         val updated = current + 1
         if (uid.compareAndSet(current, updated)) return updated
         generateAccountId
     }
 
-  private def processTransactions: Unit = {
-      
-  }
+    private def processTransactions: Unit = {
+        while (!transactionsQueue.isEmpty) {
+            val nextTransaction = transactionsQueue.pop
+            executorContext.execute(nextTransaction)
+        }
+    }
 
-  def addAccount(initialBalance: Double): Account = {
-    new Account(this, initialBalance)
-  }
+    def addAccount(initialBalance: Double): Account = {
+        new Account(this, initialBalance)
+    }
 
-  def getProcessedTransactionsAsList: List[Transaction] = {
-    processedTransactions.iterator.toList
-  }
+    def getProcessedTransactionsAsList: List[Transaction] = {
+        processedTransactions.iterator.toList
+    }
 
 }
